2d96616e6fdb55cd7d1b408cbc2d71114e9dbfa6
sangwon0707 <sangwon07@gmail.com>
2025-09-02 09:35:53 +0900

    Update homework.py

--- 2025-09-01/homework.py
+++ 2025-09-01/homework.py
@@ -1,9 +1,17 @@
 #부가세 + 팁(20%)
 VAT = 0.19
-price_before = float(input("상품가격: ") )
+TIP = 0.20
+price_before = float(input("상품가격: "))
 tax = price_before * VAT
-price_final = price_before + tax
+tip = price_before * TIP
+
+price_final = price_before + tax + tip
+
+print("부가세:", tax)
+print("팁:", tip)
 print("판매가격:", price_final)
+
+
 
 #화씨를 섭씨로 변환
 fah = float(input("화씨 온도는?"))
